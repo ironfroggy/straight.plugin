34edc625aa7980311cc405171ea9a0c0baff9d6a
Dustin Lacewell <dustin@contactmaverick.com>
2011-04-04 09:25:56 -0500

    Added distutils setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='straight.plugin',
+      version='1.0',
+      description='A simple namespaced plugin facility',
+      author='Calvin Spealman',
+      author_email='ironfroggy@gmail.com',
+      url='https://github.com/ironfroggy/straight.plugin',
+      packages=['straight', 'straight.plugin'],
+     )
