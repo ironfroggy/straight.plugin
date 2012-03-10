1e0405fd257c081c3df83be6657e7988da0222af
Calvin Spealman <calvin@pantechnoco.com>
2012-03-10 09:25:50 -0800

    Updated setup.py with version 1.3 and classifiers.

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@
 from distutils.core import setup
 
 setup(name='straight.plugin',
-      version='1.2',
-      description='A simple namespaced plugin facility',
-      author='Calvin Spealman',
-      author_email='ironfroggy@gmail.com',
-      url='https://github.com/ironfroggy/straight.plugin',
-      packages=['straight', 'straight.plugin'],
-     )
+    version='1.3',
+    description='A simple namespaced plugin facility',
+    author='Calvin Spealman',
+    author_email='ironfroggy@gmail.com',
+    url='https://github.com/ironfroggy/straight.plugin',
+    packages=['straight', 'straight.plugin'],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Environment :: Plugins',
+    ]
+)
