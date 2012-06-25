457551f84966705efcb2687a48fe59b60d5500b2
Calvin Spealman <calvin@pantechnoco.com>
2012-06-25 19:29:49 -0400

    Mark properly as a namespace package

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ except ImportError:
     INSTALL_REQUIRES.append('importlib')
 
 setup(name='straight.plugin',
-    version='1.3.1-2',
+    version='1.3.1-3',
     description='A simple namespaced plugin facility',
     author='Calvin Spealman',
     author_email='ironfroggy@gmail.com',
     url='https://github.com/ironfroggy/straight.plugin',
-    packages=['straight', 'straight.plugin'],
+    packages=['straight.plugin'],
+    namespace_packages=['straight'],
     install_requires=INSTALL_REQUIRES,
     classifiers=[
         'Programming Language :: Python :: 2',
